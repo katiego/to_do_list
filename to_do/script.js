409ca59d7570d6a54b88f52910eccb9841fe91c2
Katie Gaudin <katiegaudin2@GaudinMacintosh.local>
2015-06-29 18:37:03 -0700

    added description

--- to_do/script.js
+++ to_do/script.js
@@ -3,12 +3,14 @@ $(function() {
 var $newTaskForm = $("#new_task");
 var $doneList = $("#currentTaskList")
 console.log('$newTaskForm');
+var taskList = [{task: "walk dog", desctiption: "today"}, {task: "clean room", desctiption: "today"}]
 
 $newTaskForm.on("submit", function(event) {
 	event.preventDefault();
-	var newLI = $('#task_name').val();
-	console.log(newLI)
-	$("#currentTaskList").append("<li class='task'>" +newLI + "</li>");
+	var newTask = $('#task_name').val();
+	var newDescription = $('#task_description').val();
+	console.log(newTask)
+	$("#currentTaskList").append("<li class='task'> task: " + newTask + "   description: " + newDescription + "</li>");
 	$doneList = $(".task")
 	$doneList.click(function(event) {
 		$(this).addClass("done")
@@ -18,8 +20,8 @@ $newTaskForm.on("submit", function(event) {
 	});
 
 // store our new shopping list item
-currentTaskList.push(newLI)
-console.log(currentTaskList)
+// currentTaskList.push(newTask, newDescription)
+// console.log(currentTaskList)
 
 });
 
